f8dbd797b05d8791dcbe8a313943ef50e1047c4e
Giovane Barcelos <giovanebarcelos@gmail.com>
2025-04-08 11:17:59 -0300

    doc: diagrama de classes da gestão de comandas (M001)

--- src/aula05/M001App.java
+++ src/aula05/M001App.java
@@ -9,6 +9,70 @@ Peixe 230cal Sorvete diet 110cal Suco de Laranja 70cal
 Frango 250cal Mousse diet 170cal Suco melão 100cal
 Carne 350cal Mouse chocolate 200cal Refrigerante diet 65cal
  */
+/*
+PlantUML
+@startuml
+class M001App {
+  + main()
+}
+
+interface ItemComanda {
+}
+
+class Comanda {
+  + Comanda()
+  - ArrayList<ItemComanda> menu;
+  - ArrayList<ItemComanda> comanda;
+
+  + void fazerPedido()
+  - int getTotalCalorias()
+  - void lerItemComanda(
+         Class ItemComanda)
+  - void listarItemsComanda(
+         Class tipoItemComanda)
+  - ItemComanda getItemComanda(
+         Class tipo, int codigo)
+
+}
+
+class Prato {
+  + String descricao
+  + int calorias
+  + int codigo
+
+  + Prato( int codigo, String descricao,
+          int calorias )
+}
+
+class Sobremesa {
+  + String descricao
+  + int calorias
+  + int codigo
+
+  + Sobremesa( int codigo, String descricao,
+          int calorias )
+}
+
+class Bebida {
+  + String descricao
+  + int calorias
+  + int codigo
+
+  + Bebida( int codigo,String descricao,
+          int calorias )
+}
+
+
+Prato ..|> ItemComanda
+Sobremesa ..|> ItemComanda
+Bebida ..|> ItemComanda
+Comanda --> ItemComanda
+M001App --> Comanda : usa
+
+
+@enduml
+ */
+
 public class M001App {
     public static void main(String[] args) {
         Comanda comanda = new Comanda();
